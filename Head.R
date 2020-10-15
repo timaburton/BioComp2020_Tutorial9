e208b8c42f3c2e4b02bec7f93aa01de5a6ca1055
Anoop Sunkara <anoopsunkara@anoops-mbp.dhcp.nd.edu>
2020-10-15 16:15:33 -0400

    final add part 1

--- Head.R
+++ Head.R
@@ -1,4 +1,9 @@
-#x is the variable that represents the dataset you want the firs ten lines of
-#this functions returns the first ten lines of the data
-x  = setofdata
-head (x,10)
+#x is the variable that represents the dataset you want to subset
+#this functions returns "lines" number of lines up to and including the "end" mark
+x  = setofdata #iris
+#number of lines returned
+lines = 10
+#end mark for lines returned
+end = 11
+#printing the subset of lines up to and including the line corresponding to "end"
+print(x[(end-lines+1):end,])
