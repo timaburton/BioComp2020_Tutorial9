6312e7a5bf3fb6c7fc00269def919473ea00c5e4
Anoop Sunkara <anoopsunkara@anoops-mbp.dhcp.nd.edu>
2020-10-15 16:15:47 -0400

    final add part 2

--- irisscript.R
+++ irisscript.R
@@ -1,6 +1,6 @@
 #part1
 #make new matrix, dimensions, with the dimensions of the iris matrix
-#number of rows is in the first column and number of columns is in the second column
+#number of rows is in the first row and number of columns is in the second row
 dimensions = matrix(dim(iris))
 #a subset of rows and columns are called from iris based on the dimensions of the iris matrix
 #using the values in the first and second row of dimensions, the number of rows and columns in iris respectively
@@ -9,22 +9,22 @@ iris[(dimensions[1,]-1):dimensions[1,],(dimensions[2,]-1):dimensions[2,]]
 
 
 #part2
-#make observations matrix with just column 4 from iris that has species observations
+#make observations matrix with just column 5 from iris that has species observations
 observations = matrix(iris[,dimensions[2,]])
 
-#search for first species, setosa, and find the dimensions of the matrix 
+#search for first species, setosa, and find the dimensions of the matrix of matches
 #that is produced when setosa is searched for by grep
 setosaobs = dim(matrix(grep("setosa", observations)))
 #first item is the number of columns, or the number of setosa species observed
 setosa[1]
 
-#search for second species, versicolor, and find the dimensions of the matrix 
+#search for second species, versicolor, and find the dimensions of the matrix of matches
 #that is produced when versicolor is searched for by grep
 versicolorobs = dim(matrix(grep("versicolor", observations)))
 #first item is the number of columns, or the number of versicolor species observed
 versicolor[1]
 
-#search for third and last species, virginica, and find the dimensions of the matrix 
+#search for third and last species, virginica, and find the dimensions of the matrix of matches
 #that is produced when virginica is searched for by grep
 virginicaobs = dim(matrix(grep("virginica", observations)))
 #first item is the number of columns, or the number of virginica species observed
@@ -45,6 +45,7 @@ write.csv(iris[grep("setosa", iris$Species),], "~/Downloads/Biocomputing/BioComp
 
 #part5
 #calculating mean, minimum, and maximum of Petal.length for the observations of species virginica
+#identified virginica matches and examine column 3 for Petal.length
 #mean
 mean(iris[grep("virginica", iris$Species),3])
 #minimum
